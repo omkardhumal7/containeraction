79dfae02bde998eefe6de524e76fdd90e99655b1
Omkar Dhumal <85798584+omkardhumal7@users.noreply.github.com>
2026-06-30 16:53:23 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "Hello $MY_INPUT"
 memory=$(cat /proc/meminfo)
-#echo "::set-output name=memory::$memory"
-echo "{name}={memory}" >> "$memory"
+echo "::set-output name=memory::$memory"
+#echo "{name}={memory}" >> "$memory"
